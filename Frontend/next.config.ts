f40816c0bd46e5021d1387b8761d7ed32c259865
Elisabeth Ryan <elisabeth.ryan@dekiru.se>
2025-08-14 11:36:37 +0200

    Temporarily disable lint settings on build

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 🚫 Don't run ESLint during production builds TODO Ta bort
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
