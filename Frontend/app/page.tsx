771ffa6d607e5ad969bfa78533ad4dc3315a3ac3
Elisabeth Ryan <elisabeth.ryan@dekiru.se>
2025-08-17 12:59:34 +0200

    Redesign startpage to not use old list component

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,11 +1,10 @@
 import styles from "./page.module.css";
-import ListView from "../components/listView/ListView";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <h1>Elisabeths lilla app</h1>
-      <ListView />
+      <p>Detta är startsida, gå till /story för att komma vidare</p>
     </div>
   );
 }
