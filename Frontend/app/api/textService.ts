3dbf12df75a0263625504a53d2f2f54de95fac34
Elisabeth Ryan <elisabeth.ryan@dekiru.se>
2025-09-07 05:35:23 +0200

    refactor textservice

--- Frontend/app/api/textService.ts
+++ Frontend/app/api/textService.ts
@@ -4,22 +4,19 @@ export async function fetchText(
   id: string
 ): Promise<{ text: FormattedLetter[] } | undefined> {
   try {
-    // Use your deployed backend URL
     const backendUrl =
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
     const res = await fetch(`${backendUrl}/text/${id}`);
 
     if (!res.ok) {
       console.error("Backend returned an error:", res.status, res.statusText);
-      return undefined; // Return null instead of throwing
+      return undefined;
     }
 
     const data = await res.json();
-    console.log("Fetched data:", data);
-
     return data;
   } catch (err) {
-    console.error("Failed to fetch text:", err);
-    return undefined; // Prevents crashing SSR
+    console.error("Failed to fetch letter and convert it to json", err);
+    return undefined;
   }
 }
