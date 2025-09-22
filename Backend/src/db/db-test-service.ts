3adbfe99545fe3372de8a92d5ce6ba71e201858d
Elisabeth Ryan <elisabeth.ryan@dekiru.se>
2025-09-22 20:59:04 +0200

    Added production setup for postgres connection

--- Backend/src/db/db-test-service.ts
+++ Backend/src/db/db-test-service.ts
@@ -7,15 +7,23 @@ export class DbTestService {
   constructor(private configService: ConfigService) {}
 
   async checkConnection() {
-    const client = new Client({
-      host: this.configService.get<string>('DB_HOST', 'localhost'),
-      port: Number(this.configService.get<number>('DB_PORT', 5432)),
-      user: this.configService.get<string>('POSTGRES_USER'),
-      password: this.configService.get<string>('POSTGRES_PASSWORD'),
-      database: this.configService.get<string>('POSTGRES_DB'),
-    });
+    const environment = this.configService.get<string>('ENVIRONMENT');
+    const client =
+      environment === 'Development'
+        ? new Client({
+            host: this.configService.get<string>('DB_HOST', 'localhost'),
+            port: Number(this.configService.get<number>('DB_PORT', 5432)),
+            user: this.configService.get<string>('POSTGRES_USER'),
+            password: this.configService.get<string>('POSTGRES_PASSWORD'),
+            database: this.configService.get<string>('POSTGRES_DB'),
+          })
+        : new Client({
+            connectionString: this.configService.get<string>('DATABASE_URL'),
+            ssl: { rejectUnauthorized: false }, // Railway requires SSL
+          });
 
     try {
+      console.log('Connecting to database in environment: ', environment);
       await client.connect();
       const result = await client.query('SELECT NOW()');
       return { message: 'Connected ✅', serverTime: result.rows[0] };
