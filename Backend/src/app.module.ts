563727ed6b0dd4b17bae52e3adf49503ef86e646
Elisabeth Ryan <elisabeth.ryan@dekiru.se>
2025-10-26 18:16:59 +0100

    Add prisma for making db tables

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TextModule } from './text/text.module';
 import { ConfigModule } from '@nestjs/config';
+import { LetterModule } from './letter/letter.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TextModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TextModule, LetterModule],
   controllers: [AppController],
   providers: [AppService],
 })
