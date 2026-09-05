9c5bcb376532e9c433540f0dd9cb921b07a665e3
Elisabeth Ryan <elisabeth.ryan@dekiru.se>
2026-09-05 14:38:52 +0200

    refactor: add Nest Prisma service

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { TextModule } from './text/text.module';
 import { ConfigModule } from '@nestjs/config';
 import { LetterModule } from './letter/letter.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TextModule, LetterModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    TextModule,
+    LetterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
