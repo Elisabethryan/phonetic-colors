fb9c4f08a850535a57c54105617e63bc4d071978
Elisabeth Ryan <elisabeth.ryan@dekiru.se>
2026-09-13 17:08:29 +0200

    upgrade frontend packages

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    // 🚫 Don't run ESLint during production builds TODO Ta bort
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
